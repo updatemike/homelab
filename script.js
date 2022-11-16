19598e9c5596079fef9f873189911b90fa6a897e
Michael-a-Maycock <115986306+michael-a-maycock@users.noreply.github.com>
2022-11-16 21:02:16 +0000

    Minor visual changes

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 const pi1Btn = document.querySelector("#pi1-btn");
 const pi2Btn = document.querySelector("#pi2-btn");
 const odroid1Btn = document.querySelector("#odroid1-btn");
+const defaultInfo = document.querySelector(".info-default");
 const pi1Info = document.querySelector(".info-pi1");
 const pi2Info = document.querySelector(".info-pi2");
 const odroid1Info = document.querySelector(".info-odroid1");
 
 pi1Btn.addEventListener('click', () => {
+    defaultInfo.style.display = "none";
     pi1Info.style.display = "block";
     document.getElementById("pi1-btn").classList.add("btn-selected");
     pi2Info.style.display = "none";
@@ -17,6 +19,7 @@ pi1Btn.addEventListener('click', () => {
 })
 
 pi2Btn.addEventListener('click', () => {
+    defaultInfo.style.display = "none";
     pi2Info.style.display = "block";
     document.getElementById("pi1-btn").classList.remove("btn-selected");
     pi1Info.style.display = "none";
@@ -26,6 +29,7 @@ pi2Btn.addEventListener('click', () => {
 })
 
 odroid1Btn.addEventListener('click', () => {
+    defaultInfo.style.display = "none";
     pi1Info.style.display = "none";
     document.getElementById("pi1-btn").classList.remove("btn-selected");
     pi2Info.style.display = "none";

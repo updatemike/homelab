05faaa78f121bca546602c119135f745020beed1
updatemike <115986306+updatemike@users.noreply.github.com>
2022-11-18 20:26:31 +0000

    Optimize server button functions.

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
 /* Show/hide server info and Change button styling when selected */
 
-const pi1Btn = document.querySelector("#pi1-btn");
-const pi2Btn = document.querySelector("#pi2-btn");
-const odroid1Btn = document.querySelector("#odroid1-btn");
-const defaultInfo = document.querySelector("#info-default");
-const pi1Info = document.querySelector("#info-pi1");
-const pi2Info = document.querySelector("#info-pi2");
-const odroid1Info = document.querySelector("#info-odroid1");
 const infoBox = document.querySelectorAll(".info-box");
 const serverBtn = document.querySelectorAll(".server-btn")
 
-pi1Btn.addEventListener('click', () => {
-    infoBox.forEach(box => { box.classList.add("hidden") })
-    pi1Info.classList.remove("hidden");
+function pi1Click() {
+    infoBox.forEach(box => { box.classList.add("hidden") });
+    document.getElementById("info-pi1").classList.remove("hidden");
 
-    serverBtn.forEach(btn => { btn.classList.remove("btn-selected") })
+    serverBtn.forEach(btn => { btn.classList.remove("btn-selected") });
     document.getElementById("pi1-btn").classList.add("btn-selected");
-})
+}
 
-pi2Btn.addEventListener('click', () => {
+function pi2Click() {
     infoBox.forEach(box => { box.classList.add("hidden") })
-    pi2Info.classList.remove("hidden");
+    document.getElementById("info-pi2").classList.remove("hidden");
 
     serverBtn.forEach(btn => { btn.classList.remove("btn-selected") })
     document.getElementById("pi2-btn").classList.add("btn-selected");
-})
+}
 
-odroid1Btn.addEventListener('click', () => {
+function odroid1Click() {
     infoBox.forEach(box => { box.classList.add("hidden") })
-    odroid1Info.classList.remove("hidden");
+    document.getElementById("info-odroid1").classList.remove("hidden");
 
     serverBtn.forEach(btn => { btn.classList.remove("btn-selected") })
     document.getElementById("odroid1-btn").classList.add("btn-selected");
-})
+}
+
 
 /*  */
 

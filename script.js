80911d7f4f05542446f71bb5bee1fe0500848665
Michael-a-Maycock <115986306+michael-a-maycock@users.noreply.github.com>
2022-11-18 11:25:57 +0000

    Optimize button selection js

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
-/* Show/hide server info */
+/* Show/hide server info and Change button styling when selected */
 
 const pi1Btn = document.querySelector("#pi1-btn");
 const pi2Btn = document.querySelector("#pi2-btn");
 const odroid1Btn = document.querySelector("#odroid1-btn");
-const defaultInfo = document.querySelector(".info-default");
-const pi1Info = document.querySelector(".info-pi1");
-const pi2Info = document.querySelector(".info-pi2");
-const odroid1Info = document.querySelector(".info-odroid1");
+const defaultInfo = document.querySelector("#info-default");
+const pi1Info = document.querySelector("#info-pi1");
+const pi2Info = document.querySelector("#info-pi2");
+const odroid1Info = document.querySelector("#info-odroid1");
+const infoBox = document.querySelectorAll(".info-box");
+const serverBtn = document.querySelectorAll(".server-btn")
 
 pi1Btn.addEventListener('click', () => {
-    defaultInfo.style.display = "none";
-    pi1Info.style.display = "block";
+    infoBox.forEach(box => { box.classList.add("hidden") })
+    pi1Info.classList.remove("hidden");
+
+    serverBtn.forEach(btn => { btn.classList.remove("btn-selected") })
     document.getElementById("pi1-btn").classList.add("btn-selected");
-    pi2Info.style.display = "none";
-    document.getElementById("pi2-btn").classList.remove("btn-selected");
-    odroid1Info.style.display = "none";
-    document.getElementById("odroid1-btn").classList.remove("btn-selected");
 })
 
 pi2Btn.addEventListener('click', () => {
-    defaultInfo.style.display = "none";
-    pi2Info.style.display = "block";
-    document.getElementById("pi1-btn").classList.remove("btn-selected");
-    pi1Info.style.display = "none";
+    infoBox.forEach(box => { box.classList.add("hidden") })
+    pi2Info.classList.remove("hidden");
+
+    serverBtn.forEach(btn => { btn.classList.remove("btn-selected") })
     document.getElementById("pi2-btn").classList.add("btn-selected");
-    odroid1Info.style.display = "none";
-    document.getElementById("odroid1-btn").classList.remove("btn-selected");
 })
 
 odroid1Btn.addEventListener('click', () => {
-    defaultInfo.style.display = "none";
-    pi1Info.style.display = "none";
-    document.getElementById("pi1-btn").classList.remove("btn-selected");
-    pi2Info.style.display = "none";
-    document.getElementById("pi2-btn").classList.remove("btn-selected");
-    odroid1Info.style.display = "block";
+    infoBox.forEach(box => { box.classList.add("hidden") })
+    odroid1Info.classList.remove("hidden");
+
+    serverBtn.forEach(btn => { btn.classList.remove("btn-selected") })
     document.getElementById("odroid1-btn").classList.add("btn-selected");
 })
 
-/* Change button layout */
+/*  */
 
 

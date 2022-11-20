79023646c2ed881b0889ccc4511f45d196d27aa0
updatemike <115986306+updatemike@users.noreply.github.com>
2022-11-20 19:21:26 +0000

    Change scroll on click from js to html

--- script.js
+++ script.js
@@ -3,39 +3,30 @@
 //Return info to default and change focus to server selection
 
 const infoBox = document.querySelectorAll(".info-box");
-const serverBtn = document.querySelectorAll(".server-btn")
-const serverInfo = document.getElementById("serverInfo");
+const serverBtn = document.querySelectorAll(".server-btn");
 
 function pi1Click() {
-    infoBox.forEach(box => { box.classList.add("hidden") });
-    document.getElementById("info-pi1").classList.remove("hidden");
+  infoBox.forEach((box) => {box.classList.add("hidden");});
+  document.getElementById("info-pi1").classList.remove("hidden");
 
-    serverBtn.forEach(btn => { btn.classList.remove("btn-selected") });
-    document.getElementById("pi1-btn").classList.add("btn-selected");
-
-    serverInfo.scrollIntoView({ behavior: "smooth", block: "start" });
+  serverBtn.forEach((btn) => {btn.classList.remove("btn-selected");});
+  document.getElementById("pi1-btn").classList.add("btn-selected");
 }
 
 function pi2Click() {
-    infoBox.forEach(box => { box.classList.add("hidden") })
-    document.getElementById("info-pi2").classList.remove("hidden");
+  infoBox.forEach((box) => {box.classList.add("hidden");});
+  document.getElementById("info-pi2").classList.remove("hidden");
 
-    serverBtn.forEach(btn => { btn.classList.remove("btn-selected") })
-    document.getElementById("pi2-btn").classList.add("btn-selected");
-
-    serverInfo.scrollIntoView({ behavior: "smooth", block: "start" });
+  serverBtn.forEach((btn) => {btn.classList.remove("btn-selected");});
+  document.getElementById("pi2-btn").classList.add("btn-selected");
 }
 
 function odroid1Click() {
-    infoBox.forEach(box => { box.classList.add("hidden") })
-    document.getElementById("info-odroid1").classList.remove("hidden");
+  infoBox.forEach((box) => {box.classList.add("hidden");});
+  document.getElementById("info-odroid1").classList.remove("hidden");
 
-    serverBtn.forEach(btn => { btn.classList.remove("btn-selected") })
-    document.getElementById("odroid1-btn").classList.add("btn-selected");
-
-    serverInfo.scrollIntoView({ behavior: "smooth", block: "start" });
+  serverBtn.forEach((btn) => {btn.classList.remove("btn-selected");});
+  document.getElementById("odroid1-btn").classList.add("btn-selected");
 }
 
 //FIX SCROLL CANCELING scrollintoview
-
-

89c5e3e2380b482789be479a4e8148d52b68c1f6
updatemike <115986306+updatemike@users.noreply.github.com>
2022-12-02 17:10:04 +0000

    Formatter change

--- script.js
+++ script.js
@@ -6,26 +6,38 @@ const infoBox = document.querySelectorAll(".info-box");
 const serverBtn = document.querySelectorAll(".server-btn");
 
 function pi1Click() {
-  infoBox.forEach((box) => {box.classList.add("hidden");});
+  infoBox.forEach((box) => {
+    box.classList.add("hidden");
+  });
   document.getElementById("info-pi1").classList.remove("hidden");
 
-  serverBtn.forEach((btn) => {btn.classList.remove("btn-selected");});
+  serverBtn.forEach((btn) => {
+    btn.classList.remove("btn-selected");
+  });
   document.getElementById("pi1-btn").classList.add("btn-selected");
 }
 
 function pi2Click() {
-  infoBox.forEach((box) => {box.classList.add("hidden");});
+  infoBox.forEach((box) => {
+    box.classList.add("hidden");
+  });
   document.getElementById("info-pi2").classList.remove("hidden");
 
-  serverBtn.forEach((btn) => {btn.classList.remove("btn-selected");});
+  serverBtn.forEach((btn) => {
+    btn.classList.remove("btn-selected");
+  });
   document.getElementById("pi2-btn").classList.add("btn-selected");
 }
 
 function odroid1Click() {
-  infoBox.forEach((box) => {box.classList.add("hidden");});
+  infoBox.forEach((box) => {
+    box.classList.add("hidden");
+  });
   document.getElementById("info-odroid1").classList.remove("hidden");
 
-  serverBtn.forEach((btn) => {btn.classList.remove("btn-selected");});
+  serverBtn.forEach((btn) => {
+    btn.classList.remove("btn-selected");
+  });
   document.getElementById("odroid1-btn").classList.add("btn-selected");
 }
 
